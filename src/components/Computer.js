9b46484f12060a4dcdfb32eb3c86cf81d159d0a1
Tom CHARLET <charlettom11@gmail.com>
2025-02-17 21:41:08 +0100

    update

--- src/components/Computer.js
+++ src/components/Computer.js
@@ -1,5 +1,6 @@
 'use client'
 
+import { useRef, useEffect } from "react";
 import Data from "@/lib/Desktop"
 import dynamic from "next/dynamic"
 
@@ -8,9 +9,7 @@ const Icon = dynamic(() => import('./Icon'));
 const Computer = () => {
     const data = Data()
 
-    console.log(data)
-
-    return <div className="w-[80%] max-h-full aspect-video p-5 rounded-md border border-white bg-neutral-900 text-white grid grid-cols-6 grid-rows-6 text-[1vw]">
+    return <div className="w-[80%] max-h-full aspect-video p-5 rounded-md border border-white bg-neutral-900 text-white grid grid-cols-8 grid-rows-6 text-[1vw] gap-2">
         {data?.map((item, index) => <Soft key={index} {...item} />)}
     </div>
 }
@@ -18,8 +17,17 @@ const Computer = () => {
 export default Computer
 
 const Soft = ({ title, type, position, content }) => {
-    return <div className="gap-1 bg-neutral-800 h-full grid grid-rows-[1fr_auto] place-items-center text-center">
+    const ref = useRef(null)
+
+    useEffect(() => {
+        if (ref?.current && position?.x && position?.y) {
+            ref.current.style.gridColumnStart = position.x
+            ref.current.style.gridRowStart = position.y
+        }
+    }, [])
+
+    return <button ref={ref} className={`cursor-pointer h-full p-1 grid grid-rows-[1fr_auto] gap-1 place-items-center text-center rounded hover:bg-neutral-800`}>
         <Icon name={type} fill="white" className="size-full" />
         {title}
-    </div>
+    </button>
 }
